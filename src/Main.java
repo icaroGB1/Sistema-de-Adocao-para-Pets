099e4950a54622d3e4f9788c95542b4a24c1f765
Icaro Gabriel <icarowp45@gmail.com>
2025-09-13 21:14:11 -0300

    Classe Menu para controle menu implementada

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import FileUtils.Arq;
+import Service.Menu;
+
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(String.valueOf(10));
         Arq arq = new Arq();
         arq.lerArquivo();
+        System.out.println();
+        Menu menu = new Menu();
+        menu.opcoesMenu();
     }
 }
---
+++ src/Service/Menu.java
@@ -0,0 +1,39 @@
+package Service;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Menu {
+    public  int opcoesMenu(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("1 - Cadastrar um novo pet");
+        System.out.println("2 - Alterar os dados do pet cadastrado");
+        System.out.println("3 - Deletar um pet cadastrado");
+        System.out.println("4 - Listar todos os pets cadastrados");
+        System.out.println("5 - Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6 - Sair");
+        int escolha = validMenu(input);
+        return escolha;
+    }
+
+    public int validMenu (Scanner sc) {
+        while (true){
+            System.out.println("Digite um número de 1 a 6: ");
+            if(sc.hasNextInt()){
+                int num = sc.nextInt();
+            if(num >=1 && num <=6){
+                return num;
+            }else {
+                System.out.println("Número fora do intervalo. Tente novamente.");
+            }
+            }
+            else {
+                System.out.println("Entrada inválida. Digite apenas números.");
+                sc.nextLine();
+            }
+
+    }
+
+    }
+
+}
